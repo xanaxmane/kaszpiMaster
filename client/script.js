1fe617e85354a03e1fcc49c9fa564d714970694d
Clonaz <xanaxmane@gmail.com>
2023-01-30 15:22:30 +0100

    update

--- client/script.js
+++ client/script.js
@@ -98,7 +98,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://kaszpiaisupport.onrender.com', {
+    const response = await fetch('https://kaszpiserver.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
